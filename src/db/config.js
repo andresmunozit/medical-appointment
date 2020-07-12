0d1932cbedf154581cbe018ea8f558648d7278d1
Andres Munoz <am@andresmunozit.com>
2020-07-12 17:34:46 -0500

    Change of configuration to run tests on CI

--- src/db/config.js
+++ src/db/config.js
@@ -3,7 +3,7 @@ const Pool = require('pg').Pool;
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    password: process.env.POSTGRES_PASSWORD,
+    password: process.env.POSTGRES_PASSWORD  || '',
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 });
